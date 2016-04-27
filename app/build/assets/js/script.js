1f169c8655b0fe91496d6a91145162b7c16179f4
Jessica Aristimuño <jeakel@gmail.com>
2016-04-27 15:01:51 -0430

    adjustments

---
+++ app/build/assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready( function() { 
+	$('.input-category').click(function(){
+    $('.dropdown-content').toggleClass('dropdown-content-open');
+  });
+
+	$('.input-category').click(function(){
+    $('.btn-select').toggleClass('btn-select-rot');
+  });
+
+}); //--close all functions
